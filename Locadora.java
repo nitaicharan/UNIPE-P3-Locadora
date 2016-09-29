cc40522bcac89cedf6ab2957f33cc3b3702efcc3
Nitai Charan <nitaicharan@gmail.com>
2016-09-29 14:59:56 -0300

    Finalizando ambiente para trabalhar em grupo

--- Locadora.java
+++ Locadora.java
@@ -41,8 +41,7 @@ public class Locadora {
                         System.exit(0);
                 default:
                         System.out.println("opção Invalida!");
+            }
         }
     }
-        }
-
 }
