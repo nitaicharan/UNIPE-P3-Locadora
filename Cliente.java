b56fb88bf529770e36d09a11a3d43de9dd7c331a
Paulo Silva <gui8134@outlook.com>
2016-11-05 23:40:32 -0300

    Armazenar os cadastros de clientes em um ArrayList (lista dinamica), e criação do metodo de cadastro do objeto Cliente

---
+++ Cliente.java
@@ -0,0 +1,58 @@
+package locadora;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Cliente {
+
+	private String nome;
+	private int idade;
+	private int cpf;
+	private String contato;
+
+	public String getNome() {
+		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public String getContato() {
+		return contato;
+	}
+
+	public void Cadastrar() {
+	    Scanner leitor = new Scanner(System.in);
+
+		System.out.print("\n  Digite seu nome: ");
+		this.nome = leitor.nextLine();
+
+		while(true) {
+			try {
+				System.out.print("\n  Digite sua idade: ");
+				this.idade = leitor.nextInt();
+				break;
+			} catch(InputMismatchException a) {
+				System.out.print("\n  Tente novamente, digite apenas números");
+				leitor.nextLine();
+			}
+		}
+
+		while(true) {
+			try {
+			  	System.out.print("\n  Digite seu cpf: ");
+				this.cpf = leitor.nextInt();
+				break;
+			} catch(InputMismatchException b) {
+				System.out.print("\n  Tente novamente, digite apenas números");
+				leitor.nextLine();
+			}
+		}
+
+		System.out.print("\n  Digite o contato: ");
+		leitor.nextLine();
+		this.contato = leitor.nextLine();
+	}
+}
---
+++ Main.java
@@ -0,0 +1,48 @@
+package locadora;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+        ArrayList<Cliente> listCliente = new ArrayList<Cliente>();
+        int op = 0;
+
+        while(true) {
+            System.out.print("\n\n  Menu\n\n  1 - Cadastrar Veículo\n  2 - Cadastrar Cliente\n  3 - Locação de Veículo\n  4 - Listagem de veículos cadastrados\n  5 - Listagem das locações\n  6 - Listagem de veículos disponíveis\n  7 - Sair\n\n  Escolha uma opcão: ");
+            try {
+                op = leitor.nextInt();
+            } catch (InputMismatchException a) {
+                leitor.nextLine();
+            }
+
+            switch (op) {
+                case 1:
+                    break;
+                case 2:
+                    cliente.Cadastrar();
+                    listCliente.add(cliente);
+                    break;
+                case 3: // Esta listando todos os cadastros de clientes para teste
+                    for(int x = 0; x < listCliente.size(); x++){
+                        cliente = listCliente.get(x);
+                        System.out.println(x+1 + " " + cliente.getNome());
+                    }
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    System.exit(0);
+                default:
+                    System.out.println("\n\n  Opção Invalida!");
+            }
+        }
+    }
+}
